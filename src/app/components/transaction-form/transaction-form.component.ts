8d75888d14d1f18cbc391d9c82ff226108a201af
Deshev1 <stefandeshev@gmail.com>
2026-02-27 23:24:20 +0200

    Added datepicker and transaction type select to form.

--- src/app/components/transaction-form/transaction-form.component.ts
+++ src/app/components/transaction-form/transaction-form.component.ts
@@ -3,11 +3,21 @@ import { Component } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { ButtonModule } from 'primeng/button';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { DatePickerModule } from 'primeng/datepicker';
 
 @Component({
   selector: 'bud-transaction-form',
-  imports: [InputTextModule, InputNumberModule, ButtonModule],
+  imports: [
+    InputTextModule,
+    InputNumberModule,
+    ButtonModule,
+    SelectButtonModule,
+    DatePickerModule,
+  ],
   templateUrl: './transaction-form.component.html',
   styleUrl: './transaction-form.component.scss',
 })
-export class TransactionFormComponent {}
+export class TransactionFormComponent {
+  transactionOptions = ['Expense', 'Income'];
+}
